f8d261794668902f5a5d7668547db2610b4189d3
Hong Jiang <lazyseq@gmail.com>
2023-09-09 01:03:36 +0800

    add sha256

--- Formula/envel.rb
+++ Formula/envel.rb
@@ -2,6 +2,7 @@ class Envel < Formula
   desc "CLI tool to save environment variables to an Elisp file"
   homepage "https://github.com/hjiang/envel"
   url "https://github.com/hjiang/envel/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "614ade0a24513d79e0cd7a9600aaefb95825012c9b2b9fc02f810e1857a42de5"
 
   depends_on "cmake" => :build
 
